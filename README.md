2438d1c96a77d38db3e868e70ff66c04971dda2c
Lewis Christie <lewis.christie@gmail.com>
2015-09-10 13:24:38 -0600

    add script for checking into google api location in slack

---
+++ README.md
@@ -0,0 +1,41 @@
+# checkin-osx
+
+This package allows a user to send a location checkin message to the [slack](https://slack.com/) channel of their choice.
+
+## Installation
+
+You must register a Google API key with Places search and Reverse Geocoding services enabled.
+
+You must register a Slack API key.
+
+```
+npm install
+cp .env-example .env
+```
+
+Edit values of .env with your keys and preferences.
+
+LOCALE_TYPE should be a valid Google Reverse Geocode location type. Defaults to 'neighbourhood'.
+
+## Usage
+
+```
+npm start
+```
+Sends a message to the configured slack channel checking you in to the nearest region of the type configured.
+
+e.g.
+
+`npm start` **Lewis checked in near Whittier, Boulder, CO, USA (Thu Sep 10 2015 13:10:15 GMT-0600 (MDT))**
+
+
+```
+npm start [placeType]
+```
+
+Sends a message to the configured slack channel checking you in to the nearest found Google Place which matches the given placeType
+
+
+e.g.
+
+`npm start cafe` **Lewis checked in near Jetty Espressoria, 2116 Pearl Street, Boulder (Thu Sep 10 2015 13:14:04 GMT-0600 (MDT))**
---
+++ checkin.js
@@ -0,0 +1,94 @@
+'use strict'
+
+require('envoodoo')()
+var geolocate = require('geolocate')
+var https = require('https')
+
+var allowedTypes = require('./placesTypes.json')
+
+var googleApiKey = process.env.GOOGLE_API_KEY
+var slackApiKey = process.env.SLACK_API_KEY
+var channelId = process.env.CHANNEL_ID
+var username = process.env.USERNAME
+var botName = process.env.BOT_NAME || 'LocationBot'
+var localeType = process.env.LOCALE_TYPE || 'neighborhood'
+
+if (!googleApiKey || !slackApiKey || !channelId || !username) {
+  console.error('GOOGLE_API_KEY, SLACK_API_KEY, CHANNEL_ID and USERNAME must be set in .env or passed as environment variables')
+  process.exit()
+}
+
+function notifySlack (locationString) {
+  var msg = encodeURIComponent(username + ' checked in near ' + locationString + ' (' + (new Date(Date.now())).toString() + ')')
+  var slackUrl = 'https://slack.com/api/chat.postMessage?token=' + slackApiKey + '&channel=' + channelId + '&username=' + botName + '&text=' + msg
+  https.get(slackUrl).on('error', function (e) {
+    console.error(e)
+  })
+}
+
+function getJSON (url, done) {
+  var wasError = false
+  https.get(url, function (res) {
+    var data = ''
+    res.on('data', function (d) {
+      data += d
+    })
+    res.on('end', function () {
+      var result
+      try {
+        result = JSON.parse(data)
+      } catch (e) {
+        wasError = true
+        done(e)
+      }
+      if (!wasError) {
+        done(null, result)
+      }
+    })
+  }).on('error', function (e) {
+    wasError = true
+    done(e)
+  })
+}
+
+geolocate(function (latLong) {
+  if (process.argv[2]) {
+    var requestedType = process.argv[2]
+    if (!(allowedTypes.indexOf(requestedType) >= 0)) {
+      console.error('Type must be one of ' + allowedTypes.join(', '))
+      process.exit()
+    }
+
+    var placesSearchUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latLong.join(',') + '&rankby=distance&types=' + requestedType + '&key=' + googleApiKey
+    getJSON(placesSearchUrl, function (err, places) {
+      if (err) {
+        console.error(err)
+        process.exit()
+      }
+      if (places.results && places.results[0]) {
+        var place = places.results[0]
+        notifySlack(place.name + ', ' + place.vicinity)
+      } else {
+        console.error('Could not find a ' + requestedType + ' near this location')
+      }
+    })
+  } else {
+    var reverseGeocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latLong.join(',') + '&key=' + googleApiKey
+
+    getJSON(reverseGeocodeUrl, function (err, result) {
+      if (err) {
+        console.error(err)
+        process.exit()
+      }
+      var requestedLocales = result.results.filter(function (locale) {
+        return locale.types.indexOf(localeType) >= 0 && locale.formatted_address
+      })
+
+      if (!requestedLocales[0]) {
+        console.error('Could not find a ' + localeType + ' for this location')
+      } else {
+        notifySlack(requestedLocales[0].formatted_address)
+      }
+    })
+  }
+})
